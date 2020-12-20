84b25e66ed28dcab84c26574b0bf38d1ea8ad08a
viktor <radchenkovictor@yahoo.com>
2020-12-20 20:18:06 +0400

    flask app name

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 class BaseConfig(object):
     """Base configuration."""
 
-    APP_NAME = 'Flask App'
+    APP_NAME = os.environ.get('APP_NAME', 'Flask App')
     DEBUG_TB_ENABLED = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Ensure you set a secret key, this is important!')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
